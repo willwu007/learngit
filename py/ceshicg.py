fd7a1a217a78b4a214451d300b73c3c52210c420
willwu007 <52168189+willwu007@users.noreply.github.com>
2019-09-20 15:51:39 +0800

    Add files via upload

---
+++ py/ceshicg.py
@@ -0,0 +1,8 @@
+n=123
+f=456.789
+s1='hello,world'
+s2='hello,\'adam\''
+s3=r'hello,"Bart"'
+s4=r'''hello,
+lisa!'''
+print('n =',n,'\nf =',f,'\ns1 = ',s1,'\ns2 = ',s2,'\ns3 = ',s3,'\ns4 = ',s4)
---
+++ py/name3.py
@@ -0,0 +1,4 @@
+name = input('Please enter your name:')
+print('hello,',name)
+print('1024*768=',1024*768)
+
